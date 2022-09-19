5ec61da53ad1d2e4aaf82e1179c877d303870580
Lanfrane <archangelx66@gmail.com>
2022-09-19 21:15:28 +0700

    .

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -3,10 +3,10 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Selamat Datang. Selamat Belajar Web Programming";
-        $this->load->view('view-latihan1');
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        //$this->load->view('view-latihan1');
     }
-    public function penjumlahan($n1, $n2)
+        public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_latihan1');
         $data['nilai1'] = $n1;
